09c0b999b2deba9b103f8769d7505bcecb48bfa6
Adil Hafeez <adil@katanemo.com>
2025-12-03 17:12:34 -0800

    release 0.3.21 (#626)

--- arch/tools/cli/consts.py
+++ arch/tools/cli/consts.py
@@ -2,4 +2,4 @@ import os
 
 SERVICE_NAME_ARCHGW = "archgw"
 ARCHGW_DOCKER_NAME = "archgw"
-ARCHGW_DOCKER_IMAGE = os.getenv("ARCHGW_DOCKER_IMAGE", "katanemo/archgw:0.3.20")
+ARCHGW_DOCKER_IMAGE = os.getenv("ARCHGW_DOCKER_IMAGE", "katanemo/archgw:0.3.21")
