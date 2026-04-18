254d2b03bcbad838eeb12f70385e6f71d028fe35
Adil Hafeez <adil.hafeez@gmail.com>
2026-04-17 21:16:12 -0700

    release: bump version to 0.4.20 (#897)

--- build_filter_image.sh
+++ build_filter_image.sh
@@ -1,1 +1,1 @@
-docker build  -f Dockerfile . -t katanemo/plano -t katanemo/plano:0.4.19
+docker build  -f Dockerfile . -t katanemo/plano -t katanemo/plano:0.4.20
--- cli/planoai/__init__.py
+++ cli/planoai/__init__.py
@@ -1,3 +1,3 @@
 """Plano CLI - Intelligent Prompt Gateway."""
 
-__version__ = "0.4.19"
+__version__ = "0.4.20"
